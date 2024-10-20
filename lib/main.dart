e5c19f0179e06eb10863bfaec7e518c012ca5bf0
Atharva <mvmatharva4@gmail.com>
2024-10-20 10:16:27 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:instagram_clone/constants/utils/colors.dart';
 // import 'package:instagram_clone/view/responsive/mobile_screen_layout.dart';
 // import 'package:instagram_clone/view/responsive/responsive_layout_screen.dart';
@@ -12,12 +13,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
     await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "AIzaSyAkW_6I4vyBI08bN2aH8qggmtikZ-DukHU",
-        appId: "1:161986778515:web:c915b40344cabf6ffa8466",
-        messagingSenderId: "161986778515",
-        projectId: "instagram-clone-e17ad",
-        storageBucket: "instagram-clone-e17ad.appspot.com",
+      options: FirebaseOptions(
+        apiKey: dotenv.env['FIREBASE_API_KEY']!,
+        appId: dotenv.env['FIREBASE_APP_ID']!,
+        messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+        storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
       ),
     );
   } else {
