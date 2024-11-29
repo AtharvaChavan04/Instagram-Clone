476b8eb2d0ac461204bcf0d5155f988a4875a246
Atharva <mvmatharva4@gmail.com>
2024-11-29 12:51:11 +0530

    first commit

---
+++ lib/model/data_models/post_models.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Posts {
+  final String description;
+  final String uid;
+  final String postId;
+  final String username;
+  final datePublished;
+  final String postUrl;
+  final String profileImage;
+  final likes;
+
+  const Posts({
+    required this.description,
+    required this.uid,
+    required this.postId,
+    required this.username,
+    required this.datePublished,
+    required this.postUrl,
+    required this.profileImage,
+    required this.likes,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'description': description,
+        'uid': uid,
+        'postId': postId,
+        'username': username,
+        'datePublished': datePublished,
+        'postUrl': postUrl,
+        'profileImage': profileImage,
+        'likes': likes,
+      };
+
+  static Posts fromSnap(DocumentSnapshot snap) {
+    var snapshot = snap.data() as Map<String, dynamic>;
+
+    return Posts(
+      description: snapshot['description'],
+      uid: snapshot['uid'],
+      username: snapshot['username'],
+      postId: snapshot['postId'],
+      datePublished: snapshot['datePublished'],
+      postUrl: snapshot['postUrl'],
+      profileImage: snapshot['profileImage'],
+      likes: snapshot['likes'],
+    );
+  }
+}
---
+++ lib/model/resources/firestore_methods.dart
@@ -0,0 +1,46 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagram_clone/model/data_models/post_models.dart';
+import 'package:instagram_clone/model/resources/storage_methods.dart';
+import 'package:uuid/uuid.dart';
+
+class FirestoreMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  //upload post
+  Future<String> uploadPost(
+    String description,
+    Uint8List file,
+    String uid,
+    String username,
+    String profileImage,
+  ) async {
+    String res = "Some Error Occured!";
+    try {
+      String photoUrl =
+          await StorageMethods().uploadImageToStorage('posts', file, true);
+
+      String postId = const Uuid().v1();
+
+      Posts posts = Posts(
+        description: description,
+        uid: uid,
+        postId: postId,
+        username: username,
+        datePublished: DateTime.now(),
+        postUrl: photoUrl,
+        profileImage: profileImage,
+        likes: [],
+      );
+
+      _firestore.collection('posts').doc(postId).set(
+            posts.toJson(),
+          );
+      res = "sucess";
+    } catch (er) {
+      res = er.toString();
+    }
+    return res;
+  }
+}
