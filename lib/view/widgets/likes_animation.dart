6acba468382c7d538c9cfbbe7ee045874db2c259
Atharva <mvmatharva4@gmail.com>
2024-12-13 17:45:38 +0530

    Fixed Some Bugs

---
+++ lib/view/widgets/likes_animation.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class LikesAnimation extends StatefulWidget {
+  final Widget child;
+  final bool isAnimating;
+  final Duration duration;
+  final VoidCallback? onEnd;
+  final bool smallLike;
+  const LikesAnimation({
+    super.key,
+    required this.child,
+    required this.isAnimating,
+    this.duration = const Duration(milliseconds: 150),
+    this.onEnd,
+    this.smallLike = false,
+  });
+
+  @override
+  State<LikesAnimation> createState() => _LikesAnimationState();
+}
+
+class _LikesAnimationState extends State<LikesAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scale;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: widget.duration.inMilliseconds ~/ 2,
+      ),
+    );
+    scale = Tween<double>(begin: 1, end: 1.2).animate(controller);
+  }
+
+  @override
+  void didUpdateWidget(covariant LikesAnimation oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.isAnimating != oldWidget.isAnimating) {
+      startAnimation();
+    }
+  }
+
+  startAnimation() async {
+    if (widget.isAnimating || widget.smallLike) {
+      await controller.forward();
+      await controller.reverse();
+      await Future.delayed(
+        const Duration(milliseconds: 200),
+      );
+
+      if (widget.onEnd != null) {
+        widget.onEnd!();
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: scale,
+      child: widget.child,
+    );
+  }
+}
